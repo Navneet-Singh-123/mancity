c78d04f7e7ad979c0a5cf921b7571206a2437f06
Navneet Singh <53988436+Navneet-Singh-123@users.noreply.github.com>
2020-09-05 10:30:44 +0530

    Created Header

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import "./Resources/css/app.css";
+
+import { BrowserRouter } from "react-router-dom";
+import Routes from "./routes";
+
+// Whatever goes inside the browser router is goin to be our main application in this case it is routes.js
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
